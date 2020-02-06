904003223a56295094d36ac67befd55b311515b2
Nathan Hensel <nathanihensel@gmail.com>
2020-02-06 05:28:09 +0000

    import elasticsearch cluster

---
+++ elasticsearch.tf
@@ -0,0 +1,14 @@
+resource "lxd_container" "haproxy-elasticsearch" {
+  count     = 2
+  name      = "haproxy-elasticsearch-${count.index}"
+  image     = "alpine/stable"
+  provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
+}
+
+resource "lxd_container" "elasticsearch" {
+  count     = 2
+  name      = "elasticsearch-${count.index}"
+  image     = "alpine/stable"
+  provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
+  limits = { cpu = 2 }
+}
