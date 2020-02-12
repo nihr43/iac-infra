e1f647145cf1a1ceff6c934362e25fd8886937bb
Nathan Hensel <nathanihensel@gmail.com>
2020-02-12 04:04:00 +0000

    persist state in etcd

--- providers.tf
+++ providers.tf
@@ -1,6 +1,14 @@
 variable "K8S_PASS" {}
 variable "LXD_PASS" {}
 
+terraform {
+  backend "etcdv3" {
+    endpoints = ["10.0.0.54:2379"]
+    lock      = true
+    prefix    = "terraform-state/"
+  }
+}
+
 provider "lxd" {
   generate_client_certificates = true
   accept_remote_certificate    = true
@@ -15,7 +23,7 @@ provider "lxd" {
 }
 
 provider "kubernetes" {
-  host                   = "https://10.0.0.246:6443"
+  host                   = "https://10.0.0.56:6443"
   username               = "admin"
   password               = var.K8S_PASS
   cluster_ca_certificate = file("~/.kube/cluster-ca-cert.pem")
