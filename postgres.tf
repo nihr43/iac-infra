649240c2bbc88ea9cae7c33919df09121ac91310
Nathan Hensel <nathanihensel@gmail.com>
2020-03-14 13:58:38 +0000

    style nit

--- postgres.tf
+++ postgres.tf
@@ -1,7 +1,7 @@
 resource "lxd_container" "postgres" {
-  count     = 1
-  name      = "postgres-${count.index}"
-  image     = "alpine/stable"
-  provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
+  count  = 1
+  name   = "postgres-${count.index}"
+  image  = "alpine/stable"
   limits = { cpu = 2 }
+  provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
 }
