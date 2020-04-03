dc68293e15b9b54615ee9b5e1af2292b3a91c80a
Nathan Hensel <nathanihensel@gmail.com>
2020-04-03 22:27:28 +0000

    sytle

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,14 +1,13 @@
 resource "lxd_container" "haproxy-elasticsearch" {
-  count     = 2
-  name      = "haproxy-elasticsearch-${count.index}"
-  image     = "alpine/stable"
+  count = 2
+  name  = "haproxy-elasticsearch-${count.index}"
+  image = "alpine/stable"
   provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
 }
 
 resource "lxd_container" "elasticsearch" {
-  count     = 2
-  name      = "elasticsearch-${count.index}"
-  image     = "alpine/stable"
+  count = 2
+  name  = "elasticsearch-${count.index}"
+  image = "alpine/stable"
   provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
-  limits = { cpu = 2 }
 }
