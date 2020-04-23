bf8b3dcc2ea8bbcf8f78e6efcabbdba0c7f342e6
Nathan Hensel <nathanihensel@gmail.com>
2020-04-23 05:54:22 +0000

    deploy postgres

--- postgres.tf
+++ postgres.tf
@@ -2,6 +2,5 @@ resource "lxd_container" "postgres" {
   count  = 1
   name   = "postgres-${count.index}"
   image  = "alpine/stable"
-  limits = { cpu = 2 }
-  provisioner "local-exec" { command = "scripts/utilities.sh provision ${self.name}" }
+  provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
 }
