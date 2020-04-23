23915379c7bb52a86741d4240e0bf5dc119b79fe
Nathan Hensel <nathanihensel@gmail.com>
2020-04-23 05:53:34 +0000

    flip-flop minio

--- minio-green.tf
+++ minio-green.tf
@@ -1,5 +1,5 @@
 resource "lxd_container" "minio-green" {
-  count  = 6
+  count  = 4
   name   = "minio-green-${count.index}"
   image  = "alpine/stable"
   provisioner "local-exec" { command = "scripts/provision_alpine.sh ${self.name}" }
